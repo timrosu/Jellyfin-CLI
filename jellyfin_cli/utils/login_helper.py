abd1f0c820d00498bb18b45bb4dee761ac3f948b
Tim <49987073+timrosu@users.noreply.github.com>
2024-04-27 18:46:30 +0200

    move credentials file to ~/.config/jellyfin_cli

--- jellyfin_cli/utils/login_helper.py
+++ jellyfin_cli/utils/login_helper.py
@@ -2,6 +2,7 @@ from jellyfin_cli.jellyfin_client.JellyfinClient import HttpClient, InvalidCrede
 from aiohttp import ClientSession
 from json import load, dump
 from getpass import getpass
+import os
 
 async def login():
     try:
@@ -25,17 +26,26 @@ async def login():
             continue
 
 def store_creds(context):
-    with open("creds.json", "w") as f:
+    home_dir = os.environ['HOME']
+    conf_dir = f'{home_dir}/.config/jellyfin_cli'
+    if not os.path.exists(conf_dir):
+        os.mkdir(conf_dir)
+    creds_file = f'{conf_dir}/creds.json'
+    with open(creds_file, "w") as f:
         dump({
             "url": context.url,
             "auth_header": context.client._default_headers["x-emby-authorization"],
             "user_id": context.user_id,
             "server_id": context.server_id,
             "username": context.username
-        },f)
+        }, f)
+
 
 async def load_creds():
-    with open("creds.json", "r") as f:
+    home_dir = os.environ['HOME']
+    conf_dir = f'{home_dir}/.config/jellyfin_cli'
+    creds_file = f'{conf_dir}/creds.json'
+    with open(creds_file, "r") as f:
         dc = load(f)
         client = HttpClient(dc["url"], ServerContext(cfg=dc))
         await client.test_token()
